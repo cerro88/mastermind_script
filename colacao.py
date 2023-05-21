29c298a574899598c7ab611a3950ceae5e32c60c
cerro88 <bcerro245@gmail.com>
2023-05-21 09:31:32 +0200

    esjercicios corregidos

---
+++ colacao.py
@@ -0,0 +1,15 @@
+print("Voy a la cocina")
+print("Abro la nevera")
+leche = input("¿Hay leche? (s/n): ")
+if leche != "s":
+    print("No hay leche,voy a comprar...")
+colacao = input("¿Hay colacao? (s/n): ")
+if colacao != "s":
+    print("No queda colacao, voy a comprar...")
+
+
+print("Pongo dos cucharadas de colacao en el vaso...")
+print("Pongo dos dedos de leche...")
+print("Mezclo...")
+print("Termino de rellenar el vaso con leche...")
+print("Mmmmm...")
---
+++ conversor_libras_euros.py
@@ -0,0 +1,5 @@
+print("CONVERSOR DE LIBRAS A EUROS")
+GBP=float(input("Libras: "))
+EURO=float(input("Valor del Euro: "))
+print("€ {}".format(GBP*EURO))
+#SE USA FLOAT EN VEZ DE INT, YA QUE FLOAT NOS PERMITE USAR DECIMALES
