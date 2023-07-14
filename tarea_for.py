c2174d7b83282a1a6b3f960d698899d8f58be005
cerro88 <131131936+cerro88@users.noreply.github.com>
2023-07-14 13:59:59 +0200

    intento

--- tarea_for.py
+++ tarea_for.py
@@ -3,3 +3,16 @@
 # por ejemplo “Hola me llamo Nate, ¿tú cómo te llamas?”  
 # que busque y señale cuántos espacios,
 # puntos y comas existen en la frase.
+
+caracteres = ["",".",","]
+
+string = "Hola, Soy Belén y estoy hasta el moño."
+
+caracteres_encontrados = 0
+
+for caracteres in string:
+    if caracteres in string:
+        caracteres_encontrados += 1
+print("caracteres encontrados: {}".format(caracteres_encontrados))
+
+#no esta bien. algo falla, repasar antes de corregir
